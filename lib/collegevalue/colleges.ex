03ab055f887fee5f9edc30c70d85a3367c161f7c
mhyrr <grolsen@gmail.com>
2020-02-08 09:20:05 -0500

    Separate fields context

--- lib/collegevalue/colleges.ex
+++ lib/collegevalue/colleges.ex
@@ -4,7 +4,6 @@ defmodule Collegevalue.Colleges do
   """
 
   import Ecto.Query, warn: false
-  alias Ecto.Adapters.SQL
   alias Collegevalue.{Repo, Pagination}
   alias Collegevalue.Colleges.College
 
@@ -239,28 +238,5 @@ defmodule Collegevalue.Colleges do
     Discipline.changeset(discipline, %{})
   end
 
-  alias Collegevalue.Colleges.Field
-
-  def list_fields do
-
-    Repo.all(Field)
-
-    # This needs to be another schema/view
-
-    # Dump this into an ets table later
-    # Another select d.debt_mean, d.earnings, c.name, d.credential_level  from disciplines d, colleges c where d.college_id = c.id and d.name = 'Business/Commerce, General.' order by earnings DESC;
-
-  end
-
-  def list_fields(col, order) when is_binary(col) and is_binary(order) do
-
-    sort = String.to_atom(col)
-    sort_order = String.to_existing_atom(order)
-
-    Field
-    |> order_by({^sort_order, ^sort} )
-    |> Repo.all()
-
-  end
 
 end
