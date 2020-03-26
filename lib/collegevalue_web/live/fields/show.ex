3a6c042b8f04e262a6c3dfd777de00a5b918d18d
mhyrr <grolsen@gmail.com>
2020-03-26 13:33:19 -0400

    Sorting all the colleges

--- lib/collegevalue_web/live/fields/show.ex
+++ lib/collegevalue_web/live/fields/show.ex
@@ -9,24 +9,43 @@ defmodule CollegevalueWeb.FieldsLive.Show do
 
   @spec mount(any, any) :: {:ok, any}
   def mount(_, socket) do
+    sort_order = "asc"
     cred_label = "Show All Degrees"
     toggle_cred = false;
-    {:ok, assign(socket, cred_label: cred_label, toggle_cred: toggle_cred)}
+    {:ok, assign(socket, cred_label: cred_label, toggle_cred: toggle_cred, order: sort_order)}
+  end
+
+  @spec handle_params(map, any, Phoenix.LiveView.Socket.t()) :: {:noreply, any}
+  def handle_params(%{"name" => name, "sort_by" => sort_by}, _url, socket) do
+    field = Fields.get_field!(URI.decode(name))
+
+    sort_order = case socket.assigns.order do
+      "desc" ->
+        "asc"
+      "asc" ->
+        "desc"
+      _ ->
+        "desc"
+    end
+
+    majors = case sort_by do
+      sort_by when sort_by in ~w(name credential debt_mean debt_median earnings) ->
+        Colleges.get_by_field(field.name) |> sort_majors(sort_by) |> handle_direction(sort_order)
+      _ ->
+        Colleges.get_by_field(field.name)
+    end
+
+    {:noreply, socket |> assign(field: field) |> assign(majors: majors) |> assign(order: sort_order)}
   end
 
   @spec handle_params(map, any, Phoenix.LiveView.Socket.t()) :: {:noreply, any}
   def handle_params(%{"name" => name}, _url, socket) do
-
     field = Fields.get_field!(URI.decode(name))
-    # IO.inspect(name)
-
     majors = Colleges.get_by_field(field.name)
 
-    IO.inspect(majors)
-
     {:noreply, socket |> assign(field: field) |> assign(majors: majors)}
-
   end
+
 
   def handle_event("toggle_credentials", _value, socket) do
 
@@ -41,6 +60,67 @@ defmodule CollegevalueWeb.FieldsLive.Show do
     {:noreply, new_socket}
 
   end
+
+
+  def handle_direction(majors, order) do
+    case order do
+      "desc" ->
+        Enum.reverse(majors)
+      _ ->
+        majors
+    end
+  end
+
+  def sort_majors(majors, "name") do
+    majors
+    |> Enum.sort_by(fn major  -> major.college_name end)
+  end
+
+
+  def sort_majors(majors, "credential") do
+    majors
+    |> Enum.sort_by(fn major  -> major.credential_level end)
+  end
+
+  def sort_majors(majors, "debt_mean") do
+    majors
+    |> Enum.map( fn x ->
+      case Map.get(x, :debt_avg) do
+        nil ->
+          Map.put(x, :debt_avg, 0)
+        _ ->
+          x
+      end
+    end)
+    |> Enum.sort_by(fn major  -> major.debt_mean end, &Decimal.cmp(&1, &2) != :lt)
+  end
+
+  def sort_majors(majors, "debt_median") do
+    majors
+    |> Enum.map( fn x ->
+      case Map.get(x, :debt_min) do
+        nil ->
+          Map.put(x, :debt_min, 0)
+        _ ->
+          x
+      end
+    end)
+    |> Enum.sort_by(fn major  -> major.debt_median end, &Decimal.cmp(&1, &2) != :lt)
+  end
+
+  def sort_majors(majors, "earnings") do
+    majors
+    |> Enum.map( fn x ->
+      case Map.get(x, :earn_min) do
+        nil ->
+          Map.put(x, :earn_min, 0)
+        _ ->
+          x
+      end
+    end)
+    |> Enum.sort_by(fn major  -> major.earnings end, &Decimal.cmp(&1, &2) != :lt)
+  end
+
 
 
 end
