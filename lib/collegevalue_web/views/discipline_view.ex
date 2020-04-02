03be70da8b55ddd1b6fa952d735501396398ead2
mhyrr <grolsen@gmail.com>
2020-04-02 15:42:00 -0400

    cleaan up colleges, need to switch to a liveview

--- lib/collegevalue_web/views/discipline_view.ex
+++ lib/collegevalue_web/views/discipline_view.ex
@@ -1,3 +1,21 @@
 defmodule CollegevalueWeb.DisciplineView do
   use CollegevalueWeb, :view
+
+  alias Collegevalue.Credential
+
+  def cash(dollars) do
+    case dollars do
+      0 ->
+        nil
+      -1 ->
+        nil
+      _ ->
+        Number.Currency.number_to_currency(dollars)
+    end
+  end
+
+  def degree(level) do
+    Credential.lookup(level)
+  end
+
 end
