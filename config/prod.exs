d55b8e7a935d705d8f2840758d3cd9327a6ae10b
mhyrr <grolsen@gmail.com>
2020-04-06 16:58:39 -0400

    app name

--- config/prod.exs
+++ config/prod.exs
@@ -54,7 +54,7 @@ config :logger, level: :info
 # and configuration from environment variables.
 import_config "prod.secret.exs"
 
-config :gigalixir_getting_started, CollegevalueWeb.Endpoint,
+config :collegevalue, CollegevalueWeb.Endpoint,
 http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
 url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
 secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
